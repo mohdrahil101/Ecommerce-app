43cafa0e27149a75170c41ac8522a4e9b84938ae
Mohd. Rahil <mdrahil327@gmail.com>
2025-06-18 09:50:43 +0530

    first commit

---
+++ Components/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='container-fluid d-flex flex-column border border-secondary p-3 align-items-center w-50 mx-auto'>
+        <h2 className='mb-3'>Sign in to Continue</h2>
+        <table className='p-3'>
+            <tr className=''>
+                <td className=''>Username:</td><td><input type="text" name="" id="" /></td>
+            </tr>
+            <tr>
+                <td>Password:</td><td><input type="password" name="" id="" /></td>
+            </tr>
+        </table>
+        <input className='btn btn-lg btn-primary mt-3' type="submit" value="Submit" />
+    </div>
+  )
+}
+
+export default Login
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from '../Components/Login'
+import Home from '../Components/Home'
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
