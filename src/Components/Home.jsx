64d45bae15009c54dfa6001ec0328c5fb4b43ec4
Mohd. Rahil <mdrahil327@gmail.com>
2025-06-18 11:57:59 +0530

    login form data capture

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,8 +5,6 @@ const Home = () => {
   return (
     <div className='container text-center'>
         <h1>Welcome to ECommerce App</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
     </div>
   )
 }
